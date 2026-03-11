e0e7c0b55cc566ec54b8012b460b3fff5ea881bf
Brayan <mrx.source.mrx@gmail.com>
2026-03-11 15:45:38 -0500

    feat: add global configuration constants

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const importanceColors = {
+  none: "border-border",
+  low: "border-blue-500",
+  medium: "border-yellow-500",
+  high: "border-red-500",
+}
+
+export const importanceLabels = {
+  none: "Not assigned",
+  low: "Low",
+  medium: "Medium",
+  high: "High",
+}
